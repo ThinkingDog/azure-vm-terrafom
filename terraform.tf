994c84c3f6f2d0a898995164e8a4cd79d221dced
David Barringer <nukdcbear@gmail.com>
2021-05-03 13:07:46 -0400

    Initial project commit

---
+++ terraform.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.50.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-dev-rg"
+    storage_account_name = "terradevtfstates"
+    container_name       = "tfstates"
+    key                  = "luna-dev/luna-dev.terraform.tfstate"
+  }
+  #   backend "remote" {
+  #     organization = "Thinking-Dog" # org name from step 2.
+  #     workspaces {
+  #       name = "myTestTFstates" # name for your app's state.
+  #     }
+  #   }
+  # }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
