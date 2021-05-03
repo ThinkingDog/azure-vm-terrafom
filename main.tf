8540ba6664a1c59eaedb211778323518b80c182c
David Barringer <nukdcbear@gmail.com>
2021-05-03 14:23:29 -0400

    Fix typo in OS sku

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04.0-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
