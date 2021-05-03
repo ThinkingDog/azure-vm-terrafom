890b738f332fbe9604de601bb9fcd0b2c276c9e4
David Barringer <nukdcbear@gmail.com>
2021-05-03 13:49:37 -0400

    Missed a variable

--- variables.tf
+++ variables.tf
@@ -17,3 +17,9 @@ variable "tenant_id" {
 variable "public_key" {
 
 }
+
+variable "public_ip_allowlist" {
+  description = "List of public IP addresses to allow into the network."
+  type        = list(any)
+  default     = []
+}
